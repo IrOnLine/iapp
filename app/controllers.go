88b5f881562ad170062a3f2e93eec0b4ad86c7ca
IrOnLine <abro@yandex.ru>
2023-08-11 18:36:36 +0300

    дополнил OpenAPI и обработчики

---
+++ app/controllers.go
@@ -0,0 +1,107 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/johndoe/api/models"
+)
+
+// CreateUser creates a new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	// get request body
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	// validate request
+	if user.Email == "" {
+		http.Error(w, "Email is required", http.StatusBadRequest)
+		return
+	}
+
+	// create user in db
+	err := models.CreateUser(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// return 201 created
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "User created"}`))
+}
+
+// CreateGood handler
+func CreateGood(w http.ResponseWriter, r *http.Request) {
+
+	var good Good
+	// decode request into good model
+
+	err := models.CreateGood(good)
+	// save good to db
+
+}
+
+// CreateOrder handler
+func CreateOrder(w http.ResponseWriter, r *http.Request) {
+
+	var order Order
+	// decode request into order model
+
+	err := models.CreateOrder(order)
+	// save order to db
+
+}
+
+// GetUsers gets all users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+
+	// get users from db
+	users, err := models.GetUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// return users as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+// GetUser gets a single user by id
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+	// get id from request
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// get user from db
+	user, err := models.GetUser(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// return user as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+// GetGood handler
+func GetGood(w http.ResponseWriter, r *http.Request) {
+
+	// get goodId parameter
+	// fetch good from db
+	// encode good as JSON response
+
+}
+
+// GetOrder handler
+func GetOrder(w http.ResponseWriter, r *http.Request) {
+
+	// get orderId parameter
+	// fetch order from db
+	// encode order as JSON response
+
+}
--- app/model.go
+++ app/model.go
@@ -1,92 +1,135 @@
 package models
 
 import (
-  "database/sql"
+	"database/sql"
 
-  _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // User model
 type User struct {
-  ID int    
-  Name string
-  Email string
+	ID    int
+	Name  string
+	Email string
 }
 
 // CreateUser creates a new user in the database
 func CreateUser(user User) error {
 
-  // connect to db
-  db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
-  if err != nil {
-    return err
-  }
-  defer db.Close()
+	// connect to db
+	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-  // insert user
-  res, err := db.Exec("INSERT INTO users VALUES (?, ?, ?)", user.ID, user.Name, user.Email)
-  if err != nil {
-    return err
-  }
+	// insert user
+	res, err := db.Exec("INSERT INTO users VALUES (?, ?, ?)", user.ID, user.Name, user.Email)
+	if err != nil {
+		return err
+	}
 
-  // get ID of created user
-  id, err := res.LastInsertId()
-  if err != nil {
-    return err
-  }
+	// get ID of created user
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
 
-  // set ID on user model
-  user.ID = int(id)
+	// set ID on user model
+	user.ID = int(id)
 
-  return nil
+	return nil
 }
 
 // GetUsers gets all users from the database
 func GetUsers() ([]User, error) {
-  
-  // connect to db
-  db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
-  if err != nil {
-    return nil, err
-  }
-  defer db.Close()
 
-  // query db
-  rows, err := db.Query("SELECT * FROM users")
-  if err != nil {
-    return nil, err
-  }
+	// connect to db
+	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-  // scan rows into user models
-  var users []User
-  for rows.Next() {
-    var user User
-    err := rows.Scan(&user.ID, &user.Name, &user.Email)
-    if err != nil {
-      return nil, err
-    }
-    users = append(users, user)
-  }
+	// query db
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
 
-  return users, nil
+	// scan rows into user models
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
 }
 
 // GetUser gets a single user by ID from the database
 func GetUser(id string) (User, error) {
 
-  // connect to db  
-  db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
-  if err != nil {
-    return User{}, err
-  }
-  defer db.Close()
+	// connect to db
+	db, err := sql.Open("mysql", "user:password@tcp(db:3306)/dbname")
+	if err != nil {
+		return User{}, err
+	}
+	defer db.Close()
 
-  // query db
-  var user User
-  err = db.QueryRow("SELECT * FROM users WHERE id=?", id).Scan(&user.ID, &user.Name, &user.Email)
-  if err != nil {
-    return User{}, err
-  }
+	// query db
+	var user User
+	err = db.QueryRow("SELECT * FROM users WHERE id=?", id).Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		return User{}, err
+	}
 
-  return user, nil
+	return user, nil
+}
+
+// Good model
+type Good struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float64
+}
+
+// Counterparty model
+type Counterparty struct {
+	ID   int
+	Name string
+	Type string // supplier or vendor
+}
+
+// Order model
+type Order struct {
+	ID     int
+	UserID int
+	Items  []OrderItem
+	Total  float64
+}
+
+// OrderItem model
+type OrderItem struct {
+	GoodID   int
+	Quantity int
+}
+
+// Warehouse model
+type Warehouse struct {
+	ID       int
+	Name     string
+	Location string
+}
+
+// Invoice model
+type Invoice struct {
+	ID      int
+	OrderID int
+	Total   float64
 }
